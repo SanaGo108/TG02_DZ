c9854c8c6ea2c2d86547fa27492e301bb4d89270
SanaGo108 <oksana135@mail.ru>
2024-11-05 14:36:46 +0300

    add new

--- main.py
+++ main.py
@@ -1,73 +1,48 @@
-import logging
 import os
-import asyncio
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+from aiogram import F
 from googletrans import Translator
-from pydub import AudioSegment
-from pydub.utils import which
-from aiogram.types import FSInputFile
 from config import TOKEN
 
-# Настройка логирования
-logging.basicConfig(level=logging.INFO)
-
-# Установка пути к ffmpeg
-AudioSegment.converter = which("ffmpeg")
-if AudioSegment.converter is None:
-    raise RuntimeError("ffmpeg not found. Make sure ffmpeg is installed and available in PATH.")
-
-# Создание объекта бота
+# Initialize bot and dispatcher
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 translator = Translator()
 
-# Убедимся, что директория img существует
-if not os.path.exists('img'):
-    os.makedirs('img')
+# Handler for /start command
+@dp.message(Command(commands=['start']))
+async def send_welcome(message: Message):
+    await message.answer("Привет! Я бот-помощник.")
 
-# Обработчик для фото
-async def handle_photo(message: types.Message):
+# Handler for photo messages
+@dp.message(F.photo)
+async def handle_photo(message: Message):
+    if not os.path.exists('img'):
+        os.makedirs('img')
+
     photo = message.photo[-1]
     file_info = await bot.get_file(photo.file_id)
-    file_name = os.path.join('img', f'{photo.file_id}.jpg')
+    photo_path = f'img/{photo.file_id}.jpg'
+    await bot.download_file(file_info.file_path, photo_path)
+    await message.answer("Файл сохранен")
 
-    await photo.download(file_name)
-    await message.reply("Photo saved!")
+# Handler for text messages to translate to English
+@dp.message(F.text)
+async def handle_text(message: Message):
+    text_to_translate = message.text
+    translated_text = translator.translate(text_to_translate, src='auto', dest='en').text
+    await message.answer(translated_text)
 
-# Обработчик для голосовых сообщений
-async def handle_voice(message: types.Message):
-    voice = await message.voice.get_file()
-    file_name_ogg = f'{voice.file_id}.ogg'
-    file_name_wav = f'{voice.file_id}.wav'
+# Command to send a voice message
+@dp.message(Command(commands=['sendvoice']))
+async def send_voice(message: Message):
+    voice_path = 'path_to_voice_message.ogg'  # Specify the path to your voice message
+    with open(voice_path, 'rb') as voice:
+        await message.answer_voice(voice)
 
-    await voice.download(file_name_ogg)
-
-    # Конвертируем ogg в wav
-    audio = AudioSegment.from_ogg(file_name_ogg)
-    audio.export(file_name_wav, format='wav')
-
-    await message.reply("Voice message saved and converted to WAV format!")
-
-    # Удаляем ogg файл, если это необходимо
-    os.remove(file_name_ogg)
-
-# Обработчик для текстовых сообщений
-async def handle_text(message: types.Message):
-    translated = translator.translate(message.text, dest='en')
-    await message.reply(f"Translation to English: {translated.text}")
-
-async def main():
-    # Регистрация обработчиков
-     dp.message.register(handle_photo, content_types=['photo'])
-     dp.message.register(handle_voice, content_types=['voice'])
-     dp.message.register(handle_text, content_types=['text'])
-
-     # Запуск бота
-     try:
-         await dp.start_polling(bot)
-     finally:
-         await bot.close()
-
- if __name__ == '__main__':
-     asyncio.run(main())
+# Run the bot
+if __name__ == '__main__':
+    dp.run_polling(bot)
